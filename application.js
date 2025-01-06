75a1885afa68df3cd71651265ad4295cc097576e
Potatian Empire (Tom) <tom.hagedorn78@gmail.com>
2025-01-06 23:49:26 +0100

    debugging shift engine and making the tutorial. doing both at the same time gives me time to think and understand the code i have written...

--- application.js
+++ application.js
@@ -1,7 +1,27 @@
 let app = new Sprite();
 let engine = new ShiftEngine("viewport",app);
 
-app.addText(FormattedText.parse(`"Hello World"`));
+let load = CFpresets.request("image",samples.image,"./texture.png");
+load.callNext();
+
+let preProcessor = new ChainedFunctions([
+    function () {
+        console.log("preProcessor");
+    }
+])
+
+let postProcessor = new ChainedFunctions([
+    function () {
+        console.log("postProcessor");
+    }
+])
+
+app.addPreProcessor(preProcessor);
+app.addPostProcessor(postProcessor);
+
+app.addText(FormattedText.parse(`align:center "Hello World"`));
 app.text.align = "center";
+app.addTransparency(0.5);
+app.position.x = 0;
 
 engine.start();
