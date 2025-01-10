fe01748566c91453eb885afc39b395620bd98bb5
Potatian Empire (Tom) <tom.hagedorn78@gmail.com>
2025-01-10 14:51:30 +0100

    doing some testing

--- application.js
+++ application.js
@@ -4,20 +4,31 @@ let engine = new ShiftEngine("viewport",app);
 let load = CFpresets.request("image",samples.image,"./texture.png");
 load.callNext();
 
-let preProcessor = new ChainedFunctions([
+let cf1 = new ChainedFunctions([
     function () {
-        console.log("preProcessor");
+        console.log("First chained functions");
     }
-])
+]);
 
-let postProcessor = new ChainedFunctions([
+let cf2 = new ChainedFunctions([
     function () {
-        console.log("postProcessor");
+        console.log("Second chained functions");
     }
-])
+]);
 
-app.addPreProcessor(preProcessor);
-app.addPostProcessor(postProcessor);
+let thread = new Thread();
+
+thread.merge([cf1,cf2]);
+
+let prioritizedCf = new ChainedFunctions([
+    function () {
+        console.log("Prioritized chained functions");
+    }
+]);
+
+thread.push(prioritizedCf);
+
+thread.callNext();
 
 app.addText(FormattedText.parse(`align:center "Hello World"`));
 app.text.align = "center";
